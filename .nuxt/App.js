c5a2911f2bce2affc4b6ecd87d54406728e86ace
R2BE <ysunny32@gmail.com>
2019-11-26 09:50:09 +0900

    test

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -11,8 +11,6 @@ import NuxtLoading from './components/nuxt-loading.vue'
 
 import '..\\css\\main.css'
 
-import '..\\node_modules\\vuetify\\dist\\vuetify.css'
-
 import _6f6c098b from '..\\layouts\\default.vue'
 
 const layouts = { "_default": _6f6c098b }
@@ -170,10 +168,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }

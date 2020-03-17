2fdc5269416aa329d68b4ef063927784570cb202
EinYs <ysunny32@gmail.com>
2020-03-17 15:36:33 +0900

    wip

--- components/methods/image.js
+++ components/methods/image.js
@@ -12,6 +12,35 @@ const imageMethods = {
     }
 
     return src || 'no meta image source'
+  },
+  thumbImages(item) {
+    let imgsrcArr = [];
+    if (item && item.attachment && item.attachment[0]) {
+      item.attachment.forEach((attachment) => {
+        let src = attachment.src; //기본 소스;
+
+        /* s3 데이터로 바꾸는 부분 : 사용중단
+        if (attachment.media && attachment.media.s3) {
+          let s3 = attachment.media.s3
+          if (s3.thumb && s3.thumb.location) {
+            src = s3.thumb.location;
+          } else if (s3.small && s3.small.location) {
+            src = s3.small.location;
+          }
+        }
+        */
+        imgsrcArr.push(src);
+      });
+    } else if (item.links && item.links[0]) {
+      const link = item.links[0];
+      if (link.media && link.media.s3 && link.media.s3.small) {
+        imgsrcArr.push(link.media.s3.small.location);
+      } else if (link.media) {
+        imgsrcArr.push(link.media.origin);
+      }
+    }
+
+    return imgsrcArr[0] ? imgsrcArr : null;
   }
 }
 
--- pages/i/_id.vue
+++ pages/i/_id.vue
@@ -1,37 +1,51 @@
 <template>
 	<div class="wrapper itempage">
 		<div style="margin:0 auto; max-width:720px">
-			<v-card flat class="pa-5 mb-5" :inner-html.prop="text | text">
+			<v-card flat>
+				<images :images="thumbImages" />
 			</v-card>
-      <h3>링크</h3>
-			<div v-for="(link) in item.links" :key="link._id">
-        <v-card flat>
-          <v-row :dense="$vuetify.breakpoint.smAndDown" style="height:200px; max-height:24vw; overflow: hidden; text-overflow: ellipsis;">
-            <v-col cols="4"><v-img :src="MediaSourceOfLink(link)" aspect-ratio="1.2"/></v-col>
-            <v-col cols="8">
-              <div class="caption">{{link.display_url}}</div>
-              <div class="mt-2 font-weight-bold">{{link.meta.title}}</div>
-              <div class="caption" style="">{{link.meta.description}}</div>
-            </v-col>
-          </v-row >
 
-        </v-card>
-        {{link}}
+			<v-card flat class="pa-5 mb-2" :inner-html.prop="text | text">
+
+			</v-card>
+      <a :href="`https://twitter.com/${item._user.name}/status/${item.id}`" target="_blank">
+      <v-btn style="width:100%" rounded depressed color="white"><v-icon color="blue">mdi-twitter</v-icon></v-btn>
+
+      </a>
+			<div class="mt-4" v-if="item && item.links && Array.isArray(item.links) && item.links[0]">
+
+				<h3>링크된 페이지</h3>
+				<div v-for="(link) in item.links" :key="link._id">
+					<v-card flat>
+						<v-row no-gutters style="overflow: hidden; text-overflow: ellipsis;">
+							<v-col cols="4">
+								<v-img v-if="MediaSourceOfLink(link)" :src="MediaSourceOfLink(link)" aspect-ratio="1.2" />
+							</v-col>
+							<v-col cols="8" class="pa-3">
+								<div class="caption">{{link.display_url}}</div>
+								<div v-if="link.meta">
+									<div class="mt-2 font-weight-bold">{{link.meta.title}}</div>
+									<div class="caption" style="">{{link.meta.description}}</div>
+								</div>
+							</v-col>
+						</v-row>
+
+					</v-card>
+					{{link}}
+				</div>
 			</div>
-        {{item.links}}
+			{{item.links}}
 
-      <div>
+			<div>
+
+			</div>
+
+			<div class="pa-5">
+				{{item}}
+
+			</div>
 
 		</div>
-
-		<div class="pa-5">
-			{{item}}
-
-		</div>
-    <div class="pa-5">
-      {{item.links}}
-    </div>
-	</div>
 	</div>
 </template>
 <script lang="ts">
@@ -39,7 +53,7 @@ import Vue from "vue";
 import Component from "vue-class-component";
 import ItemCard from "@/components/item/Card.vue";
 import Images from "@/components/item/Images.vue";
-import MImage from "@/components/methods/image"
+import MImage from "@/components/methods/image";
 
 @Component({
   filters: {
@@ -54,25 +68,33 @@ import MImage from "@/components/methods/image"
     }
   },
   async asyncData({ params, $axios }) {
-    const data = await $axios.$get("/1.0/data/items/" + params.id).then(res => {
-      return res;
-    });
+    const data = await $axios
+      .$get("/1.0/data/items/" + params.id, { timeout: 5000 })
+      .then(res => {
+        return res;
+      });
     return { item: data };
   },
   head() {
     return {
       title: `${this.item._user.name}의 커미션`
     };
+  },
+  components: {
+    Images
   }
 })
 export default class ItemPage extends Vue {
-  item:any;
+  item: any;
 
-  get text(){
-    return this.item? this.item.text : null
+  get text() {
+    return this.item ? this.item.text : null;
   }
-  MediaSourceOfLink( link ){
-    return MImage.getMediaSourceOfLink(link)
+  MediaSourceOfLink(link) {
+    return MImage.getMediaSourceOfLink(link);
+  }
+  get thumbImages() {
+    return MImage.thumbImages(this.item);
   }
 }
 </script>

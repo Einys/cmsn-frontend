702b895de0a12b946069b0297baed2e8ca228f08
einys <ysunny32@gmail.com>
2021-04-19 04:08:26 +0900

    beta

--- server/index.js
+++ server/index.js
@@ -30,18 +30,7 @@ console.log("[server] dotenv enabled : ", process.env.DOTENV_ENABLED);
  * snbot
  *
  */
-let Snbot = require("snbot");
-if (
-	process.env.SNBOT_LOCAL &&
-	process.env.SNBOT_LOCAL !== "0" &&
-	process.env.SNBOT_LOCAL_ENABLED &&
-	process.env.SNBOT_LOCAL_ENABLED !== "0"
-) {
-	console.log("[server] snbot local", process.env.SNBOT_LOCAL);
-	Snbot = require(process.env.SNBOT_LOCAL); // '../../snbot/dist'
-} else {
-	console.log("[server] snbot npm");
-}
+let Snbot = require("@einys/snbot");
 
 /**
  *
--- server/routes/route.js
+++ server/routes/route.js
@@ -4,24 +4,12 @@ var router = express.Router();
 var createError = require('http-errors');
 var bodyParser = require('body-parser');
 
-let ItemController = require('snbot/data/controller.item')
-let BotController = require('snbot/data/controller.bot')
-let AuthController = require('snbot/data/controller.auth')
-let UserController = require('snbot/data/controller.user')
-let DataController = require('snbot/data/controller.data')
-let ChatEventController = require('snbot/data/controller.chatevent')
-
-if (process.env.SNBOT_LOCAL && process.env.SNBOT_LOCAL !== '0' && process.env.SNBOT_LOCAL_ENABLED && process.env.SNBOT_LOCAL_ENABLED !== '0') {
-  console.log('[server] local router enabled')
-  ItemController = require('../' + process.env.SNBOT_LOCAL + '/data/controller.item')
-  BotController = require('../' + process.env.SNBOT_LOCAL + '/data/controller.bot')
-  AuthController = require('../' + process.env.SNBOT_LOCAL + '/data/controller.auth')
-  UserController = require('../' + process.env.SNBOT_LOCAL + '/data/controller.user')
-  DataController = require('../' + process.env.SNBOT_LOCAL + '/data/controller.data')
-  ChatEventController = require('../' + process.env.SNBOT_LOCAL + '/data/controller.chatevent')
-} else {
-  console.log('[server] npm module router enabled')
-}
+let ItemController = require('@einys/snbot/data/controller.item')
+let BotController = require('@einys/snbot/data/controller.bot')
+let AuthController = require('@einys/snbot/data/controller.auth')
+let UserController = require('@einys/snbot/data/controller.user')
+let DataController = require('@einys/snbot/data/controller.data')
+let ChatEventController = require('@einys/snbot/data/controller.chatevent')
 
 /* GET data. */
 router.use('/items', ItemController.default);

5cee2d7c15f49f2c43341411265c7c1a35f47a74
EinYs <ysunny32@gmail.com>
2021-12-31 04:32:19 +0900

    default layout style fix

--- components/cat/Vert.vue
+++ components/cat/Vert.vue
@@ -1,9 +1,8 @@
 <template>
-	<v-container justify-center style="text-align:center; max-width:320px; padding:0;">
-		<v-col class="mb-3">
-			<v-btn text to="/t/all/open">
+	<v-container justify-center style="text-align:center; max-width:320px; padding:0; background-color: rgba(232, 233, 236, 1); border-radius:4px;">
+		<v-col class="mb-4">
+			<v-btn text class="mb-2" to="/t/all/open">
 				<h2 style="cursor:pointer">커미션 열었어요</h2>
-
 			</v-btn>
 			<v-layout row align-center justify-space-around style="font-size: 20px;">
 				<v-btn color="blue-grey" text :to="{path:'/t/art/open'}">
@@ -25,27 +24,28 @@
 			</v-layout>
 		</v-col>
 
-		<v-btn text to="/t/all/find">
-			<h2 style="cursor:pointer">커미션 찾습니다</h2>
+		<v-col>
+			<v-btn text class="mb-2" to="/t/all/find">
+				<h2 style="cursor:pointer;">커미션 찾습니다</h2>
+			</v-btn>
+			<v-layout row align-center justify-space-around>
+				<v-btn color="blue-grey" text :to="{path:'/t/art/find'}">
+					<v-icon left>mdi-satellite</v-icon>그림
+				</v-btn>
+				<v-btn color="blue-grey darken-2" text :to="{path:'/t/wri/find'}">
+					<v-icon left>mdi-text</v-icon> 글..
+				</v-btn>
 
-		</v-btn>
-		<v-layout row align-center justify-space-around>
-			<v-btn color="blue-grey" text :to="{path:'/t/art/find'}">
-				<v-icon left>mdi-satellite</v-icon>그림
-			</v-btn>
-			<v-btn color="blue-grey darken-2" text :to="{path:'/t/wri/find'}">
-				<v-icon left>mdi-text</v-icon> 글..
-			</v-btn>
-
-		</v-layout>
-		<v-layout row align-center justify-space-around>
-			<v-btn color="blue-grey darken-2" text :to="{path:'/t/des/find'}">
-				<v-icon left>mdi-shape</v-icon> 디자인..
-			</v-btn>
-			<v-btn color="blue-grey" text :to="{path:'/t/mus/find'}">
-				<v-icon left>mdi-music-note</v-icon> 음악..
-			</v-btn>
-		</v-layout>
+			</v-layout>
+			<v-layout class="mb-2" row align-center justify-space-around>
+				<v-btn color="blue-grey darken-2" text :to="{path:'/t/des/find'}">
+					<v-icon left>mdi-shape</v-icon> 디자인..
+				</v-btn>
+				<v-btn color="blue-grey" text :to="{path:'/t/mus/find'}">
+					<v-icon left>mdi-music-note</v-icon> 음악..
+				</v-btn>
+			</v-layout>
+		</v-col>
 	</v-container>
 </template>
 
@@ -58,8 +58,11 @@ export default class CatVert extends Vue {}
 </script>
 
 <style scoped>
+
 h2{
   color:rgb(44, 51, 59);
-  font-weight: 300;
+  font-weight: 400;
+  font-size: 18px;
+  font-family: "Roboto", sans-serif
 }
 </style>

d64aee9b725542357d8b24253f8d8c95d5baa25e
einys <ysunny32@gmail.com>
2022-06-17 20:01:30 +0900

    Design: 에러 페이지 디자인 변경
    새로고침 버튼 강조
    오류 플레인 로그 출력 없앰

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -7,45 +7,39 @@
       <v-card flat max-width="420">
         <v-img max-width="420" src="https://images.unsplash.com/photo-1535696588143-945e1379f1b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"></v-img>
 
-        <v-card-title>
-          <h1 v-if="error.statusCode === 404">
-            <v-icon>mdi-alert</v-icon> CODE 404 Page not found
-          </h1>
-          <h1 v-else>An Error occurred</h1>
-        </v-card-title>
         <v-card-text>
           <p>
-            어..? 예상치 못한 일이 일어났습니다.
+            어...? 문제가 발생했습니다. <br>
           </p>
-          <v-divider></v-divider>
-
-          <p>
-            {{error.stack}}
-          </p>
-          <p>
-            {{error.message}}
-          </p>
-
         </v-card-text>
-        <v-card-actions>
-          <v-btn
-            text
-            color="cyan darken-1"
-            @click="reload"
-          > 새로고침 </v-btn>
+        <v-card-title>
+          <h1 v-if="error.statusCode === 404">
+            <v-icon>mdi-alert</v-icon> 404 Page not found
+          </h1>
+        </v-card-title>
 
-          <v-spacer></v-spacer>
+        <v-card-actions>
             <v-btn
               @click="$router.back()"
               text
               color="blue-grey"
             > 뒤로가기 </v-btn>
-          <NuxtLink to="/">
+            <NuxtLink to="/">
+              <v-btn
+                text
+                color="cyan darken-1"
+              > 메인으로&nbsp;<v-icon size=16>mdi-home</v-icon></v-btn>
+            </NuxtLink>
+          <v-spacer></v-spacer>
+          <a href="https://twitter.com/cms_env">
             <v-btn
+              @click="$router.back()"
               text
-              color="cyan darken-1"
-            > 메인으로<v-icon right>mdi-home</v-icon> </v-btn>
-          </NuxtLink>
+              color="blue-grey"
+            > <v-icon size=14 color="grey">mdi-twitter</v-icon>&nbsp;관리자 트위터로 </v-btn>   
+          </a>
+       
+
         </v-card-actions>
 
       </v-card>
